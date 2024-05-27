33a62ebe1fe9ec87332232089eaeba2488f143ba
Paulino Salas <soportecpz14@gmail.com>
2024-05-27 14:54:42 -0600

    depure code

--- data_conect.ino
+++ data_conect.ino
@@ -1,19 +1,16 @@
-
 #include <WiFi.h>
 #include <WebServer.h>
 #include <HTTPClient.h>
-//sensor de temperatura------------
+// Sensor de temperatura
 #include <OneWire.h>
 #include <DallasTemperature.h>
-//Dysplay--------------------------
+// Display
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-//--------------------------------
 
-// OLED FeatherWing buttons map to different pins depending on board.
-// The I2C (Wire) bus may also be different.
+// Define los pines de los botones y el bus I2C según el tipo de placa
 #if defined(ESP8266)
   #define BUTTON_A  0
   #define BUTTON_B 16
@@ -58,62 +55,50 @@
 
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &WIRE);
 
-
-const int oneWireBus = 4;//pin
+const int oneWireBus = 4; // Pin temperatura
 OneWire oneWire(oneWireBus);
-DallasTemperature sensors (&oneWire);
+DallasTemperature sensors(&oneWire);
 
-//Snsor de vibracion
 int arrayVibrate[20];
-int hzs=0;
-int suma=0;
-int Pin_Sensor = 5; 
+int hzs = 0;
+int suma = 0;
+int Pin_Sensor = 5;
 
-
-//const char* serverUrl = "http://192.168.0.100/serverx.php";
-const char* serverUrl = "http://192.168.150.70/serverx.php";
+const char* serverUrl = "http://192.168.28.70/serverx.php";
 
 WebServer server(80);
 
 void setup() {
   Serial.begin(115200);
-//  const char* ssid     = "MERCUSYS_5EBE";
-//  const char* password = "57931289";
 
-  //const char* ssid     = "CPZ14";
-  //const char* password = "12345678";
-
-
-  const char* ssid     = "Redmi";
+  const char* ssid = "Redmi";
   const char* password = "12345678";
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-
   display.display();
-
   display.clearDisplay();
   display.display();
 
   Serial.println("Desconectamos antes de conectar el WiFi");
   WiFi.disconnect();
-  Serial.print("Conectando a  ");
+  Serial.print("Conectando a ");
   Serial.println(ssid);
-  //Conectamos el esp a la red wifi
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  //Intentamos conectarnos a la red
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  //Si logramos conectarnos mostramos la ip a la que nos conectamos
+
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-  //Si entramos a la raiz mostramos las opciones
+
   server.on("/", []() {
-    String content="";
+    String content = "";
     content += "<html>";
     content += "<div><a href=\"encender\">Encender</a></div>";
     content += "<div><a href=\"apagar\">Apagar</a></div>";
@@ -121,96 +106,82 @@ void setup() {
     server.send(200, "text/html", content);
   });
 
-  pinMode(Pin_Sensor , INPUT);
+  pinMode(Pin_Sensor, INPUT);
   server.begin();
 }
 
-void loop() {
-  //lipiar la pantalla
+void displayInfo(float temperatureC, int suma) {
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
- 
-/*
-  if(!digitalRead(BUTTON_A)) display.print("A");
-  if(!digitalRead(BUTTON_B)) display.print("B");
-  if(!digitalRead(BUTTON_C)) display.print("C");
- */
-   display.setTextSize(1);
-   display.setCursor(0,0);
-     display.print("..M E M O S C..");
-  /*sensor*/
+  display.setCursor(0, 0);
+  display.print("..M E M O S C..");
+  display.setCursor(0, 8);
+  display.print(suma);
+  display.print(" Hz");
+  display.setCursor(0, 16);
+  display.print(temperatureC);
+  display.print(" C");
+
+  display.setCursor(0, 32);
+  display.print("..M E M O S C..");
+
+  display.display();
+}
+
+void loop() {
+  
   sensors.begin();
   sensors.requestTemperatures();
   float temperatureC = sensors.getTempCByIndex(0);
   int vibrate = digitalRead(Pin_Sensor);
 
-  int sensor1=1;
-  int sensor2=2;
-  
-
-  //int valor_entero = random(0, 1000); // Generar un número aleatorio entre 0 y 999
-  //float valor = valor_entero / 100.0; // Dividir el número entero para obtener un número decimal
   Serial.print(temperatureC);
-  Serial.println("°C");
-  
+  Serial.println("C");
   Serial.print(vibrate);
- 
-  arrayVibrate[hzs]=vibrate;
 
+  arrayVibrate[hzs] = vibrate;
 
-  if(hzs==20){
-
-    hzs=0;
+  if (hzs == 20) {
+    hzs = 0;
     for (int i = 0; i < 20; i++) {
       suma += arrayVibrate[i];
     }
     Serial.print(suma);
-    Serial.println("Hz");
-    
+    Serial.println(" Hz");
 
-    // Construir el cuerpo de la solicitud HTTP
-  String data = "sensor=" + String(sensor1) + "&valor=" + String(temperatureC) + "&sensor2=" + String(sensor2)+ "&valor2=" + String(suma);
-   display.setCursor(0, 8);
-   display.print(suma);
-   display.print("Hz");
+    String data = "sensor=1&valor=" + String(temperatureC) + "&sensor2=2&valor2=" + String(suma);
 
-   display.setCursor(0, 16);
-   display.print(temperatureC);
-   display.print("°C");
+    displayInfo(temperatureC, suma);
 
-   
-  suma=0;
+    suma = 0;
 
-  HTTPClient http;
-  http.begin(serverUrl); // Especifica la URL del script PHP en el servidor
-  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    HTTPClient http;
+    http.begin(serverUrl);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
+    int httpResponseCode = http.POST(data);
 
-  int httpResponseCode = http.POST(data); // Envía los datos al servidor
+    if (httpResponseCode > 0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+      String payload = http.getString();
+      Serial.println(payload);
+    } else {
+      Serial.print("Error en la solicitud HTTP: ");
+      Serial.println(httpResponseCode);
+    }
 
-  if (httpResponseCode > 0) {
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-    String payload = http.getString();
-    Serial.println(payload);
-  } else {
-    Serial.print("Error en la solicitud HTTP: ");
-    Serial.println(httpResponseCode);
+    http.end();
+
+    server.handleClient();
+
+    //delay(100);
+    yield();
   }
 
-  http.end(); // Libera los recursos
-
-
-
-  server.handleClient();
-
-  display.display(); // actually display all of the above
-  
-  delay(100);
-  yield();
-  
-  }
-  hzs=hzs+1;
+  hzs++;
 }
+
+
 
